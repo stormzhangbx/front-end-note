a05ec691d15da54b3623e703f80dda51e33fb9cc
zhangbx <13128814051@163.com>
2018-11-05 09:54:17 +0800

    IMP: 优化

--- tip/utils.js
+++ tip/utils.js
@@ -329,6 +329,29 @@ const stamp2date = (timeStamp, seperator='/') => {
   return d
 }
 
+// 格式化时间
+const formatDate = (time, format = 'YY-MM-DD hh:mm:ss') => {
+    var date = new Date(time);
+    var year = date.getFullYear(),
+        month = date.getMonth() + 1, //月份是从0开始的
+        day = date.getDate(),
+        hour = date.getHours(),
+        min = date.getMinutes(),
+        sec = date.getSeconds();
+    var preArr = Array.apply(null, Array(10)).map(function (elem, index) {
+        return '0' + index;
+    }); // 开个长度为10的数组 格式为 00 01 02 03
+
+    var newTime = format.replace(/YY/g, year)
+        .replace(/MM/g, preArr[month] || month)
+        .replace(/DD/g, preArr[day] || day)
+        .replace(/hh/g, preArr[hour] || hour)
+        .replace(/mm/g, preArr[min] || min)
+        .replace(/ss/g, preArr[sec] || sec);
+
+    return newTime;
+}
+
 /**
  * 移除数组array中所有的元素item
  * @method removeItem
@@ -595,6 +618,7 @@ export const utils = {
   queryStr2obj,
   highLightingObj,
   stamp2date,
+  formatDate,
   removeItem,
   trim,
   isEmptyObject,
