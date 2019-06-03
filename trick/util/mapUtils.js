6c473633752b68fb258eecdbd8f63c36cb5f5324
stormzhangbx <13128814051@163.com>
2019-06-03 23:05:53 +0800

    IMP：数组和字符串

--- trick/util/mapUtils.js
+++ trick/util/mapUtils.js
@@ -405,8 +405,8 @@ export const getTotalList = (array) => {
 
 /**
  * 对象数组批量删除
- * @param {*} source 源数组
- * @param {*} deleteList 要删除的项组成的数组
+ * @param {array} source 源数组
+ * @param {array} deleteList 要删除的项组成的数组
  */
 export const batchDelete = (source, deleteList) => {
   for (let i = 0; i < source.length; i++) {
@@ -431,3 +431,36 @@ export const batchDelete = (source, deleteList) => {
 // ]
 // batchDelete(arr, deleteList)
 // console.log(arr)
+
+/**
+ * 将对象数组按某一个属性进行分组，转换成一个新的对象数组
+ * @param {array} array 对象数组
+ */
+const getTotalList1 = (array) => {
+  let result = []
+  let obj = {}
+  array.forEach(item => {
+    if (!obj[item.date]) {
+      const o = {
+        date: item.date,
+        list: [item]
+      }
+      result.push(o)
+      obj[item.date] = true
+    } else {
+      result.forEach(rItem => {
+        if (rItem.date === item.date) rItem.list.push(item)
+      })
+    }
+  })
+  return result
+}
+// var activityList = [
+//   { date: '2019-05-03', activity: '篮球' },
+//   { date: '2019-03-01', activity: '音乐' },
+//   { date: '2019-04-01', activity: '电影' },
+//   { date: '2019-04-20', activity: '书法' },
+//   { date: '2019-05-03', activity: '羽毛球' },
+//   { date: '2019-05-03', activity: '乒乓球' },
+// ]
+// console.log(getTotalList1(activityList))
